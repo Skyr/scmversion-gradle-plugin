9b9f92d30806928d210aa8d62e11f96d54f491b6
Stefan Schlott <stefan@ploing.de>
2014-02-21 20:38:01 +0100

    Create properties file with scm information

--- src/main/groovy/de/ploing/scmversion/CreateVersionFileTask.groovy
+++ src/main/groovy/de/ploing/scmversion/CreateVersionFileTask.groovy
@@ -8,6 +8,17 @@ import org.gradle.api.tasks.TaskAction
 class CreateVersionFileTask extends SCMVersionTask {
     @TaskAction
     def createVersionFile() {
-        println('Creating version file...')
+        if (project.scmversion.propertyFilename!=null) {
+            Properties props = new Properties()
+            props.setProperty('rev', scmOperations.headVersion)
+            props.setProperty('dirty', scmOperations.isRepoDirty().toString())
+            props.setProperty('version', getCurrentVersion(false))
+            File outFile = new File(project.buildDir, 'resources/main/' + project.scmversion.propertyFilename)
+            outFile.parentFile.mkdirs()
+            def out = new FileOutputStream(outFile)
+            props.store(out, 'SCM version information')
+            out.close()
+            logger.info("${outFile} created")
+        }
     }
 }
